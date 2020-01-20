1dfc9e6c2797cef186f1673424caba308aa80cf2
Yuyuuuuu <50176023+Yuyuuuuu@users.noreply.github.com>
2020-01-20 11:29:39 +0100

    Update sketch_of_eyes_part.js

--- sketch_of_eyes_part.js
+++ sketch_of_eyes_part.js
@@ -1,63 +1,69 @@
-let R;//RaduisOfEyes
-let a;
-let XPosOfEyes;
-let Y
-let CountOfClicks=0;
+var eyes=[];
 let ClickTime=[];
+let CountOfClicks=0;
+let R;
+let XPos=400;
+let YPos;
+let a;
 
-//let CurrentTime;//current time;
+var gravity = 0.1;
 
 function setup() {
    createCanvas(800, 600);
-   background(220);
-   XPosOfEyes=400; 
-}
-
-function draw() {
-  
-  //YPosOfEyes=(millis()/1000*800)/60;
-  
-  //draw the eyes
-  stroke(0);
-  strokeWeight(1);
-  fill(255);
-  //this is the white eyes par
-  
-  for(var i = 0; i < ClickTime.length ; i++) {
-    Y=(ClickTime[i+1]+ClickTime[i])/1000*7;
-    R=(ClickTime[i+1]-ClickTime[i])/1000*7;
-    
-    ellipse(XPosOfEyes , Y , R , R);
-  }
-  print(Y);
-  //this is how the black eyes part follow mouse
-  //Calculates the angle (in radians) from a specified point to the coordinate origin as measured from the positive x-axis
-
-  //this is black eyes part
-  fill(0);
-  for(var m = 0 ; m < ClickTime.length ; m++) {
-    Y=(ClickTime[m+1]+ClickTime[m])/1000*7;
-    a=atan2(mouseY-Y,mouseX-XPosOfEyes);
-    R=(ClickTime[m+1]-ClickTime[m])/1000*7;
-    
-
-    ellipse(XPosOfEyes+0.2 * R * cos(a) , Y+0.2*R*sin(a) , R * 0.5 , R * 0.5);
-    
-  //print(R);
-  } 
+   XPosOfEyes=400;
+   
+   ClickTime[0]=0; 
 }
 
 function mousePressed(){ 
-  //count how many times the mouse has been pressed
-  
-   //CurrentTime= millis();
-   
-   ClickTime.push(millis());//   add data to the array "arr"
-  
-   CountOfClicks++;
-   //print(CountOfClicks);
-   //print(millis()/1000);
-   
+  ClickTime.push(ceil(millis()/1000));
+  CountOfClicks++;
+  R=(ClickTime[CountOfClicks]-ClickTime[CountOfClicks-1])*6;
+  YPos=(ClickTime[CountOfClicks]+ClickTime[CountOfClicks-1])*6;
+  console.log(R,YPos);  
+  eyes.push(new Eye(XPos,R,YPos));
 }
 
+function draw() {
 
+  background(220); 
+  for (let i=1;i<eyes.length;i++){
+    eyes[i].whiteEyes();
+    if(keyIsPressed===true){
+    eyes[i].moveEyes();
+    eyes[i].bounceEyes();
+  }
+  } 
+}
+
+class Eye{
+
+constructor(x,r,y){
+this.x=x;
+this.r=r;
+this.y=y;
+this.speed=0;
+}
+
+whiteEyes(){
+  stroke(0);
+  strokeWeight(1);
+  fill(255);
+  ellipse(this.x,this.y,this.r,this.r);
+
+  a=a = atan2(mouseY - this.y, mouseX - this.x);
+  fill(0);
+  ellipse(this.x + 0.2 * this.r * cos(a), this.y + 0.2 * this.r * sin(a), this.r * 0.5, this.r * 0.5);
+  }
+
+moveEyes(){
+   this.y =this.y +this.speed;
+   this.speed= this.speed+gravity;
+}
+
+bounceEyes(){
+   if(this.y>height){
+     this.speed=this.speed*-1;
+   }
+}
+}
