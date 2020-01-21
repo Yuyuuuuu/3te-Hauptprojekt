06331d547b3f7133144b1aa6724adb871e5eb33c
Yuyuuuuu <50176023+Yuyuuuuu@users.noreply.github.com>
2020-01-21 09:34:44 +0100

    Update sketch_of_eyes_part.js

--- sketch_of_eyes_part.js
+++ sketch_of_eyes_part.js
@@ -1,73 +1,100 @@
-var eyes=[];//the object array of eyes
+var eyes=[];
 let ClickTime=[];
-
 let CountOfClicks=0;
-
 let R;
-let XPos=400;
+let XPos=[];
 let YPos;
-
 let a;
 
-var gravity = 0.1;
+var gravity = 0.03;
+let spring=0.05;
+let friction=-0.9;
 
 function setup() {
    createCanvas(800, 600);
-   XPosOfEyes=400;
+   XPos.push(250,260,270);
    ClickTime[0]=0; 
 }
 
 function mousePressed(){ 
   ClickTime.push(ceil(millis()/1000));
   CountOfClicks++;
-   
   R=(ClickTime[CountOfClicks]-ClickTime[CountOfClicks-1])*6;
   YPos=(ClickTime[CountOfClicks]+ClickTime[CountOfClicks-1])*6;
-   
-  console.log(R,YPos);  
-  eyes.push(new Eye(XPos,R,YPos));
+  console.log(ClickTime[CountOfClicks],R,YPos);  
+  eyes.push(new Eye(random(XPos),YPos,R,CountOfClicks,eyes));
 }
 
 function draw() {
-
   background(220); 
   for (let i=1;i<eyes.length;i++){
     eyes[i].whiteEyes();
     if(keyIsPressed===true){
+    eyes[i].collide();
     eyes[i].moveEyes();
-    eyes[i].bounceEyes();
   }
   } 
 }
 
 class Eye{
-
-constructor(x,r,y){
+constructor(x,y,r,idin,oin){
 this.x=x;
-this.r=r;
 this.y=y;
-this.speed=0;
+this.vx=0;
+this.vy=0;
+this.r=r;
+this.id=idin;
+this.others=oin;
 }
 
+collide() {
+    for (let i = this.id + 1; i < ClickTime.length-1; i++) {
+      let dx = this.others[i].x - this.x;
+      let dy = this.others[i].y - this.y;
+      let distance = sqrt(dx * dx + dy * dy);
+      let minDist = this.others[i].r/ 2 + this.r / 2;
+      if (distance < minDist) {
+        let angle = atan2(dy, dx);
+        let targetX = this.x + cos(angle) * minDist;
+        let targetY = this.y + sin(angle) * minDist;
+        let ax = (targetX - this.others[i].x) * spring;
+        let ay = (targetY - this.others[i].y) * spring;
+        this.vx -= ax;
+        this.vy -= ay;
+        this.others[i].vx += ax;
+        this.others[i].vy += ay;
+      }
+    }
+  }
+  
+  moveEyes() {
+    this.vy += gravity;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x + this.r / 2 > width) {
+      this.x = width - this.r / 2;
+      this.vx *= friction;
+    } else if (this.x - this.r / 2 < 0) {
+      this.x = this.r / 2;
+      this.vx *= friction;
+    }
+    if (this.y + this.r / 2 > height) {
+      this.y = height - this.r / 2;
+      this.vy *= friction;
+    } else if (this.y - this.r / 2 < 0) {
+      this.y = this.r / 2;
+      this.vy *= friction;
+    }
+  }
+  
 whiteEyes(){
   stroke(0);
   strokeWeight(1);
   fill(255);
-  ellipse(this.x,this.y,this.r,this.r);//this part draws the white part of eyes
-  //below draws the black part of eyes
-  a=a = atan2(mouseY - this.y, mouseX - this.x);// how the blackeyes follow mouse
+  ellipse(this.x,this.y,this.r,this.r);
+  a=a = atan2(mouseY - this.y, mouseX - this.x);
   fill(0);
   ellipse(this.x + 0.2 * this.r * cos(a), this.y + 0.2 * this.r * sin(a), this.r * 0.5, this.r * 0.5);
   }
 
-moveEyes(){
-   this.y =this.y +this.speed;
-   this.speed= this.speed+gravity;
-}
-
-bounceEyes(){
-   if(this.y>height){
-     this.speed=this.speed*-1;
-   }
-}
 }
