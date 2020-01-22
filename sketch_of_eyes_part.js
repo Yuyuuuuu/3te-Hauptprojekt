941c651d98bd684f70b4269378aba8e841a7ed08
Yuyuuuuu <50176023+Yuyuuuuu@users.noreply.github.com>
2020-01-22 09:44:08 +0100

    Update sketch_of_eyes_part.js

--- sketch_of_eyes_part.js
+++ sketch_of_eyes_part.js
@@ -16,7 +16,7 @@ function setup() {
    ClickTime[0]=0; 
 }
 
-function mousePressed(){ 
+function keyPressed(){ 
   ClickTime.push(ceil(millis()/1000));
   CountOfClicks++;
   R=(ClickTime[CountOfClicks]-ClickTime[CountOfClicks-1])*6;
@@ -29,7 +29,8 @@ function draw() {
   background(220); 
   for (let i=1;i<eyes.length;i++){
     eyes[i].whiteEyes();
-    if(keyIsPressed===true){
+    eyes[i].blackEyes();
+    if(mouseIsPressed===true){
     eyes[i].collide();
     eyes[i].moveEyes();
   }
@@ -90,11 +91,22 @@ collide() {
 whiteEyes(){
   stroke(0);
   strokeWeight(1);
-  fill(255);
+  if(keyCode===LEFT_ARROW){
+    fill(255,80,79);
+  }else if (keyCode===UP_ARROW){
+    fill(0,122,135);
+  }else if (keyCode===RIGHT_ARROW){
+    fill(64,64,255);
+  }
   ellipse(this.x,this.y,this.r,this.r);
-  a=a = atan2(mouseY - this.y, mouseX - this.x);
+  
+  }
+blackEyes(){
+  a = atan2(mouseY - this.y, mouseX - this.x);
   fill(0);
   ellipse(this.x + 0.2 * this.r * cos(a), this.y + 0.2 * this.r * sin(a), this.r * 0.5, this.r * 0.5);
-  }
+
+}
+
 
 }
