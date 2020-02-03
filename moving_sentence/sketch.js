b411225fa8f19485177592ce00ecb6dec5acc0b2
Yuyuuuuu <50176023+Yuyuuuuu@users.noreply.github.com>
2020-02-03 22:47:14 +0100

    Update sketch.js

--- moving_sentence/sketch.js
+++ moving_sentence/sketch.js
@@ -6,17 +6,18 @@ var sentence=[];
 let m=[0,1,2,3];
 let fontLight;
 
-function preload(){
- fontLight=loadFont('fonts/Quicksand-Light.ttf');
-}
+// function preload(){
+//  fontLight=loadFont('fonts/Quicksand-Light.ttf');
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  Tx = 0; 
+  Tx = -400; 
 }
 
 function draw() {
   background(200);
+  console.log(windowHeight);
   for (let l=0;l<sentence.length;l++){
       sentence[l].displayText();
       sentence[l].moveText();
@@ -24,29 +25,31 @@ function draw() {
 }
 
 function keyPressed(){
-  Ty = random(0,windowHeight);  
-  sentence.push(new Text(Tx,Ty,random(m)));
+  Ty = ceil(random(0,28))*35;  
+  sentence.push(new Text(Tx,Ty,random(m),random(0,1.5)));
 }
 
 class Text{
- constructor(tx,ty,tw){
+
+ constructor(tx,ty,tw,tv){
   this.tx=tx;
   this.ty=ty;
   this.tw=tw;
-
+  this.tv=tv;
  }
   
   displayText(){
-  textFont(fontLight);
-  fill(92);
-  stroke(92);
+  // textFont(fontLight);
+  textFont("Futura PT Light");
+  fill(150);
+  stroke(150);
   strokeWeight(this.tw);
-  textSize(40);
+  textSize(25);
   text('An 8-second message is shared.',this.tx,this.ty);
   }
   
   moveText(){
-  this.tx=this.tx+1;
+  this.tx=this.tx+this.tv;
   if (this.tx>windowWidth){
      this.tx=0;
   }
