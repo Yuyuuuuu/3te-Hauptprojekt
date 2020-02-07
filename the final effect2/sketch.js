faa88b0ac5acedce0c3151e86abef5ad9c289334
Yuyuuuuu <50176023+Yuyuuuuu@users.noreply.github.com>
2020-02-07 22:01:39 +0100

    Update sketch.js

--- the final effect2/sketch.js
+++ the final effect2/sketch.js
@@ -1,37 +1,39 @@
-//the posenet part
+var eyes=[];//the whole array of objects(eyes)
+
+let R=[];
+let Raduis;
+let XPos;
+let YPos=0;
+
+// let a;
+let v;
+//the value about moving
+var gravity = 0.005;
+let spring=0.1;
+let friction=-0.5;
+
+//the varible about the pattern
+let Tx,Ty;
+let E;
+
+// the varible about tracking
 let video;
 let poseNet;
 let pose;
 
-//the follow eyes part
-let wD; //the diameter of white eyes
-let pupils; //the diameter of pupils
-let wV;
-let wX;
-let wY;
-let bX;
-let bY;
-
-//about the array of objects eyes
-var eyes=[];
-let R=[];
+let CountOfClicks=0;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  video= createCapture(VIDEO);
-  video.hide();
-  poseNet=ml5.poseNet(video,modelLoaded);
-  poseNet.on('pose',gotPoses);
+   createCanvas(windowWidth, windowHeight);
+   R.push(20,40,60,80,100);
+   Tx=0;
+   Ty=height/2;
 
-  smooth ();
-  R.push(20,40,60,80,100);
-  wD=2*random(R);
-  wX=random(width);
-  wY=0;
-  pupils=0.4*wD;
-  wV=random(0.1,0.3);
-
-  eyes.push(new Eye(wX,wY,wD,pupils,wV,eyes));
+   video= createCapture(VIDEO);
+   video.hide();
+   poseNet=ml5.poseNet(video,modelLoaded);
+   poseNet.on('pose',gotPoses);
+   
 }
 
 function gotPoses(poses){
@@ -45,43 +47,160 @@ function modelLoaded(){
   console.log('poseNet ready');
 }
 
+function keyPressed(){ 
+  Raduis=random(R);
+  XPos=random(windowWidth);
+  E=(Raduis/20);//the magenificatiom times of pattern
+
+  v=random(0.08,0.2);
+  CountOfClicks++;
+  eyes.push(new Eye(XPos,YPos,Raduis,eyes,E,v,CountOfClicks));
+}
+
 function draw() {
-  //  image(video,0,0);
-   background(120);
-   for (let i=0;i<5;i++){
-   if(pose){
-   eyes[i].drawWhiteEyes();
-   eyes[i].drawBlackEyes();
-   }
-   }
+  background(220);
+    text('a 8 seconds information was shared.',Tx,Ty);
+    Tx=Tx+1;
+    if (Tx>windowWidth){
+      Tx=0;
+    }
+    for (let i=1;i<eyes.length;i++){
+    eyes[i].whiteEyes();
+    eyes[i].blackEyes();
+    // eyes[i].redHeart();
+    //eyes[i].yellowSharing();
+    eyes[i].collide();
+    eyes[i].moveEyes();
   }
+}
 
 class Eye{
-constructor(x,y,d1,d2,v,others){
- this.x=x;
- this.y=y;
- this.d1=d1;
- this.d2=d2;
- this.v=v;
- this.others=others;
-}
+  constructor(x,y,r,others,e,wV,n){
+  this.x=x;
+  this.y=y;
+ 
+  this.vx=0;
+  this.vy=0;
+  this.r=r;
+  this.others=others;
+  this.e=e;
+  this.wV=wV;
+  this.n=n;
+  
+  }
 
-drawWhiteEyes(){
-this.x=this.x+(pose.nose.x-this.x)*this.v;
-this.y=this.y+(pose.nose.y-this.y)*this.v;
-stroke(0);
-strokeWeight(3);
-fill(255);
-ellipse(this.x,this.y,this.d1,this.d1);
-}
+  collide() {//the action of collide between eyes
+    for (let i = eyes.length+ 1; i < eyes.length-1; i++) {
+          let dx = this.others[i].x - this.x;
+          let dy = this.others[i].y - this.y;
+          let distance = sqrt(dx * dx + dy * dy);
+          let minDist = this.others[i].r + this.r;
+          if (distance < minDist) {
+            let angle = atan2(dy, dx);
+            let targetX = this.x + cos(angle) * minDist;
+            let targetY = this.y + sin(angle) * minDist;
+            let ax = (targetX - this.others[i].x) * spring;
+            let ay = (targetY - this.others[i].y) * spring;
+            this.vx -= ax;
+            this.vy -= ay;
+            this.others[i].vx += ax;
+            this.others[i].vy += ay;
+          }
+        }
+      }
+  
+  moveEyes() { //the movement of eyes
+    this.vy += gravity;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x + this.r / 2 > width) {
+      this.x = width - this.r / 2;
+      this.vx *= friction;
+    } else if (this.x - this.r / 2 < 0) {
+      this.x = this.r / 2;
+      this.vx *= friction;
+    }
+    if (this.y + this.r / 2 > height) {
+      this.y = height - this.r / 2;
+      this.vy *= friction;
+    } else if (this.y - this.r / 2 < 0) {
+      this.y = this.r / 2;
+      this.vy *= friction;
+    }
+  }
+  
+  whiteEyes(){
+    stroke(0);
+    strokeWeight(2);
+    fill(255);
 
-drawBlackEyes(){
-strokeWeight(1);
-bX=constrain(pose.nose.x, pose.nose.x-this.d2/2, pose.nose.x+this.d2/2);
-bY=constrain(pose.nose.y, pose.nose.y-this.d2/2, pose.nose.y+this.d2/2);
-fill(0);
-ellipse(bX,bY,this.d2,this.d2);
-}
+      this.x=this.x+(pose.nose.x-this.x)*this.wV;
+      this.y=this.y+(pose.nose.y-this.y)*this.wV;
+     let wwX=constrain(this.x,(windowWidth/4)*(this.n-2)+this.r,(windowWidth/4)*(this.n-1)-this.r);
+      //let wwY=constrain(this.y,(windowWidth/4)*j,(windowWidth/4)*(j+1));
+      ellipse(wwX,this.y,2*this.r,2*this.r);
+   
 
+  //   for(let j=0;j<4;j++){
+  //   this.x=this.x+(pose.nose.x-this.x)*this.wV;
+  //   this.y=this.y+(pose.nose.y-this.y)*this.wV;
+  //   let wwX=constrain(this.x,(windowWidth/4)*j,(windowWidth/4)*(j+1));
+  //   //let wwY=constrain(this.y,(windowWidth/4)*j,(windowWidth/4)*(j+1));
+  //   ellipse(wwX,this.y,2*this.r,2*this.r);
+  // }
+  }
+
+  blackEyes(){
+
+  fill(0);
+  this.x=pose.nose.x;
+  this.y=pose.nose.y;
+  let bX=constrain(this.x, (windowWidth/4)*(this.n-2)+1.4*this.r, (windowWidth/4)*(this.n-1)-1.4*this.r);
+  let bY=constrain(this.y, pose.nose.y-0.4*this.r, pose.nose.y+0.4*this.r);
+  // let bX=constrain(this.x, pose.nose.x-0.4*this.r, pose.nose.x+0.4*this.r);
+  // let bY=constrain(this.y, pose.nose.y-0.4*this.r, pose.nose.y+0.4*this.r);
+  ellipse(bX, bY, 2*this.r * 0.4, 2*this.r * 0.4);
+  }
+
+  // redHeart(){
+  // fill(255,0,0);
+  // noStroke(0);
+  // //strokeWeight(2);
+  // push();
+  // translate(this.x + 0.2 * this.r * cos(a),this.y + 0.2 * this.r * sin(a));//keepe the position do not change
+  // scale(this.e);
+  // beginShape();
+  // vertex(0,0);//1
+  // vertex(-6,6);//2
+  // vertex(-12,0);//3
+  // vertex(-12,-4);//4
+  // vertex(-10,-6);//5
+  // vertex(-8,-6);//6
+  // vertex(-6,-4);//7
+  // vertex(-4,-6);//8
+  // vertex(-2,-6);//9
+  // vertex(0,-4);//10
+  // endShape(CLOSE);
+  // pop();
+  // }
+
+  // yellowSharing(){
+  //   fill(255,211,42);
+  //   noStroke(0);
+  //   //strokeWeight(2);
+  //   push();
+  //   translate(this.x + 0.2 * this.r * cos(a),this.y + 0.2 * this.r * sin(a));//keepe the position do not change
+  //   scale(this.e);
+  //   beginShape();
+  //   vertex(0,0);//1
+  //   vertex(-4,0);//2
+  //   vertex(-4,5);//3
+  //   vertex(-8,5);//4
+  //   vertex(-8,0);//5
+  //   vertex(-12,0);//6
+  //   vertex(-6,-7);//7
+  //   endShape(CLOSE);
+  //   pop();
+  //   }
 
 }
